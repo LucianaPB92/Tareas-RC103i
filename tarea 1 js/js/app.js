c6c978f8eb6180bbce7720ebd3755ee6357b6a9c
María Luciana Pérez Barrionuevo <lucianapbarrionuevo@hotmail.com>
2024-06-08 19:31:39 -0300

    Creacion de carpetas con tarea 1 y 2 de javascript con sus respectivos archivos html y js

---
+++ tarea 1 js/js/app.js
@@ -0,0 +1,30 @@
+//ejercicio 1
+
+let numDecimal = 0.21,
+  numEntero = 84;
+
+let suma = numDecimal + numEntero;
+let resta = numEntero - numDecimal;
+let multiplicacion = numEntero * numDecimal;
+let division = numEntero / numDecimal;
+let mayor_o_igual = numDecimal >= numEntero;
+let es_distinto = numDecimal != numEntero;
+
+console.log(suma);
+console.log(resta);
+console.log(multiplicacion);
+console.log(division);
+console.log(mayor_o_igual);
+console.log(es_distinto);
+
+//2da forma de hacerlo
+
+let numEntero1 = 20;
+let numEntero2 = 84;
+
+console.log(numEntero1 + numEntero2);
+console.log(numEntero2 - numEntero1);
+console.log(numEntero2 * numEntero1);
+console.log(numEntero1 / numEntero2);
+console.log(numEntero1 >= numEntero2);
+console.log(numEntero1 != numEntero2);
---
+++ tarea 2 js/js/app.js
@@ -0,0 +1,53 @@
+//Ejercicios de String y Números
+/*
+1 - Crea una variable con tu nombre, otra con tu apellido y concaténalas con el mensaje de bienvenida: 'Bienvenido al evento' usando template string.
+2 - Usando métodos de String quita los espacios de adelante y de atrás del siguiente texto: '              Había una vez un pequeño programador junior       '
+3 - Convierte a mayúsculas tu nombre y apellido usando las variables definidas en el punto 1
+4 - Crea un programita que le pida al usuario que ingrese un texto, convierte dicho texto en mayúsculas y muestra en consola cuantos caracteres tiene el texto recibido usando propiedades de los String.
+5 - Crea un programa que solicite al usuario ingresar un número con 5 decimales, almacenar dicho número en una variable asegurándose que sea de tipo number. Mostrar en consola lo siguiente:
+ - El número redondeado a su mayor valor
+ - El número redondeado a su menor valor
+ - El número original multiplicado por 3
+ - El resto del número dividido por 2
+*/
+
+//ejercicio 1
+/*
+let nombre = "María Luciana";
+let apellido = "Pérez Barrionuevo";
+
+console.log(`Bienvenida al evento ${nombre} ${apellido}`);
+*/
+//ejercicio 2
+
+// let frase = "              Había una vez un pequeño programador junior       ";
+// let fraseSinEspacios = frase.trim();
+
+// console.log(fraseSinEspacios);
+//2da forma
+// console.log(frase.trim());
+
+//ejercicio 3
+
+// console.log(nombre.toUpperCase(), apellido.toUpperCase());
+
+//ejercicio 4
+/*
+let ingresarTexto = prompt("Ingrese una frase")
+let fraseEnMayuscula = ingresarTexto.toUpperCase()
+console.log(fraseEnMayuscula);
+console.log(`La frase tiene ${fraseEnMayuscula.length} carácteres.`);
+*/
+//ejercicio 5
+/*
+let ingresarNumeroDecimal = prompt("Ingrese un numero con 5 decimales")
+
+let numeroIngresado = Number(ingresarNumeroDecimal)
+
+console.log(`Su número decimal redondeado a su menor valor : ${Math.ceil(numeroIngresado)}`);
+console.log(`Su número decimal redondeado a su mayor valor : ${Math.floor(numeroIngresado)}`);
+console.log(`Su numero decimal multiplicado por 3 : ${numeroIngresado*3}`);
+console.log(`El resto de su numero decimal dividido por 2 : ${numeroIngresado%2}`);
+*/
+
+
